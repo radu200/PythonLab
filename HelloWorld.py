3a0bf90c8be050192cd91294a92e51ae2ba7a367
radu200 <raduprodan200@gmail.com>
2019-10-02 23:24:59 +0100

    added lab1 HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,25 @@
+#My first Python program
+print("Hello World!")
+print('Hello World!')
+print("My lucky numbers are:", 3*4+5, 5*6-1)
+
+print("balance after  one year:", 10000*1.05)
+print("balance after  two yeara:", 10000*1.05*1.05)
+
+print("years require to double", 72/5)
+
+
+#6 
+Initialbalance = int(10000)
+Count = int(0)
+
+while(Initialbalance < 20000):
+ Initialbalance = Initialbalance * 1.05
+ count +=1
+
+print(str(count) + " years")
+
+
+
+#6.1
+print(1+2+3+4+5+6+7+8+9+10)
